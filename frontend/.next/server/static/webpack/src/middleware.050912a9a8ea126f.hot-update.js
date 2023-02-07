9adda5fac3d74e56f5843eb678467e9219b9ffc0
ZejusJS <pepitopoky.jp@gmail.com>
2023-02-07 20:20:07 +0100

    build

---
+++ frontend/.next/server/static/webpack/src/middleware.050912a9a8ea126f.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("src/middleware",{
+
+/***/ "(middleware)/./src/config/config.js":
+/*!******************************!*\
+  !*** ./src/config/config.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"server\": () => (/* binding */ server)\n/* harmony export */ });\nconst dev = \"development\" === \"production\";\nconst server = dev ? \"http://localhost:8000\" : \"http://localhost:8000\";\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKG1pZGRsZXdhcmUpLy4vc3JjL2NvbmZpZy9jb25maWcuanMuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLE1BQU1BLE1BQU1DLGtCQUF5QjtBQUU5QixNQUFNQyxTQUFTRixNQUFNLDBCQUEwQix1QkFBdUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2NvbmZpZy9jb25maWcuanM/YjRiYiJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBkZXYgPSBwcm9jZXNzLmVudi5OT0RFX0VOViA9PT0gJ3Byb2R1Y3Rpb24nXHJcblxyXG5leHBvcnQgY29uc3Qgc2VydmVyID0gZGV2ID8gJ2h0dHA6Ly9sb2NhbGhvc3Q6ODAwMCcgOiAnaHR0cDovL2xvY2FsaG9zdDo4MDAwJyJdLCJuYW1lcyI6WyJkZXYiLCJwcm9jZXNzIiwic2VydmVyIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(middleware)/./src/config/config.js\n");
+
+/***/ })
+
+});
