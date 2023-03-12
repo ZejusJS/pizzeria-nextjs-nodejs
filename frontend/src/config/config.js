ebc814c12a094d8235cdeca9f4668a5c86f0785e
ZejusJS <pepitopoky.jp@gmail.com>
2023-03-12 17:28:04 +0100

    backend urls

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV === 'production'
 
-export const server = dev ? 'http://localhost:8000' : 'http://localhost:8000'
-// export const server = dev ? 'https://pizzeria-backend-4vij.onrender.com' : 'https://pizzeria-backend-4vij.onrender.com'
+// export const server = dev ? 'http://localhost:8000' : 'http://localhost:8000'
+export const server = dev ? 'https://pizzeria-backend-4vij.onrender.com' : 'https://pizzeria-backend-4vij.onrender.com'
